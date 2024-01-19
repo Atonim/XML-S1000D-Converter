b46e99b26eaa786527b473e3e936ad10d2bcb4e1
Atonim <Atonimaurus@gmail.com>
2024-01-19 13:00:45 +0300

    Распаковка docx и преобразование в zip (LEARNING_CENTER-1280)

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,29 +1,29 @@
 <template>
-    <div class="home-page">
-      <h1>Конвертировать Word в XML S1000D</h1>
-      <Uploader
-       @drop.prevent="drop" @change="selectedFile"
-      ></Uploader>
-      <span 
-       v-if="uploaderFile"
-       class="file-info">Имя загруженного файла: <strong>{{ uploaderFile.name }}</strong></span>
-      <button 
-       class="home-page__next-btn next-bth"
-       :class="{ disabled: !uploaderFile}"
-       @click="uploaderFile ? $router.push('/getResult') : message = true"
-      >Конвертировать</button>
-      <span 
-       v-if="!uploaderFile && message">Сначала загрузите файл</span>
-    </div>
+  <div class="home-page">
+    <h1>Конвертировать Word в XML S1000D</h1>
+    <Uploader @drop.prevent="drop" @change="selectedFile"></Uploader>
+    <span v-if="uploaderFile" class="file-info"
+      >Имя загруженного файла: <strong>{{ uploaderFile.name }}</strong></span
+    >
+    <button
+      class="home-page__next-btn next-bth"
+      :class="{ disabled: !uploaderFile }"
+      @click="uploaderFile ? startConverter(uploaderFile) : (message = true)"
+    >
+      Конвертировать
+    </button>
+    <span v-if="!uploaderFile && message">Сначала загрузите файл</span>
+  </div>
 </template>
 
 <script>
-import Uploader from '@/components/Uploader';
-import { ref } from 'vue';
+import Uploader from "@/components/Uploader";
+import { startZip, startUnzip } from "@/converter/jszip";
+import { ref } from "vue";
 
 export default {
   components: {
-    Uploader
+    Uploader,
   },
   data: () => ({
     message: false,
@@ -32,16 +32,25 @@ export default {
     let uploaderFile = ref("");
 
     const drop = (e) => {
-      uploaderFile.value = e.dataTransfer.files[0]
+      uploaderFile.value = e.dataTransfer.files[0];
     };
 
     const selectedFile = () => {
-       uploaderFile.value = document.querySelector('.uploaderFile').files[0];
+      uploaderFile.value = document.querySelector(".uploaderFile").files[0];
     };
 
     return {
-      uploaderFile, drop, selectedFile
+      uploaderFile,
+      drop,
+      selectedFile,
     };
   },
-}
+  methods: {
+    startConverter(file) {
+      //startZip();
+      startUnzip(file);
+      this.$router.push("/getResult");
+    },
+  },
+};
 </script>
