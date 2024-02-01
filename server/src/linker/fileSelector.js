ec753d675337c3628a476d6e14ebe6cada8b8f3b
Atonim <Atonimaurus@gmail.com>
2024-02-01 20:46:46 +0300

    Компоновка XML файлов, полученных из конвертера (LEARNING_CENTER-1318)

--- server/src/linker/fileSelector.js
+++ server/src/linker/fileSelector.js
@@ -1,8 +1,11 @@
 export async function fileSelector(unzipped) {
 
   const keys = Object.keys(unzipped.files)
+  //console.log(unzipped)
+  //console.log(keys)
   let media = []
   let documentRels = null
+  let document = null
 
   for (let fullPath of keys) {
     if (fullPath.endsWith('.jpeg') || fullPath.endsWith('.png') || fullPath.endsWith('.enf')) {
@@ -14,10 +17,16 @@ export async function fileSelector(unzipped) {
     }
     else if (fullPath.endsWith('document.xml.rels')) {
       documentRels = await unzipped.files[fullPath].async('arraybuffer')
+      documentRels = new TextDecoder().decode(documentRels);
+
+    }
+    else if (fullPath.endsWith('document.xml')) {
+      document = await unzipped.files[fullPath].async('arraybuffer')
+      document = new TextDecoder().decode(document);
     }
     //еще есть формат enf в media
   }
-  return { media, documentRels };
+  return { media, documentRels, document };
 }
 
 const fileName = (fullPath) => {
--- server/src/linker/index.js
+++ server/src/linker/index.js
@@ -1,26 +1,26 @@
 import JSZip from "jszip";
 
-export async function linker(unzipped, xmlArray) {
+export async function linker(result) {
   const zipped = new JSZip();
   let imgFolder = zipped.folder("Images");
   let xmlFolder = zipped.folder("XML");
-  let documentRels = null;
-  const keys = Object.keys(unzipped.files)
 
-  for (let fullPath of keys) {
-    if (fullPath.endsWith('.xml')) {
-      let fileData = await unzipped.files[fullPath].async('arraybuffer')
-      xmlFolder.file(fileName(fullPath), fileData)
-    }
-    else if (fullPath.endsWith('.jpeg') || fullPath.endsWith('.png')) {
-      let fileData = await unzipped.files[fullPath].async('arraybuffer')
-      imgFolder.file(fileName(fullPath), fileData)
-    }
-    else if (fullPath.endsWith('document.xml.rels')) {
-      documentRels = await unzipped.files[fullPath].async('arraybuffer')
-    }
-    //еще есть формат enf в media
+  const xmlFileNames = Object.keys(result.XML)
+  const imgFileNames = Object.keys(result.Images)
+  //console.log(keys)
+  //console.log(result.XML[0])
+  //console.log(result.XML['DMC-VBMA-A-46-20-01-00A-018A-A_000_01_ru_RU.xml'])
+  //console.log(result.XML)
+  //console.log(result.Images)
+  //var uint8array = new TextEncoder().encode("someString");
+
+  for (let filename of xmlFileNames) {
+    //let fileData = await result.XML[filename].async('arraybuffer')
+    xmlFolder.file(filename, result.XML[filename])
   }
+  //for (let filename of imgFileNames) {
+  //  imgFolder.file(filename, result.Images[filename])
+  //}
   return zipped;
 }
 
