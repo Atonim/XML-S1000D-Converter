ebad6938fa452cdd84da4fc2188eb4e474f5e6a3
Anastasiia <anastasia_maxim@bk.ru>
2024-02-14 14:46:13 +0300

    Конвертация png изображений в jpeg - работающая версия (LEARNING_CENTER-1348)

--- server/src/linker/index.js
+++ server/src/linker/index.js
@@ -1,51 +1,48 @@
-import JSZip from "jszip";
-import { exec } from 'child_process';
-import { fileName } from '../fileName.js';
-import { sharpConverter } from "../mediaConverter.js";
+import JSZip from 'jszip'
+import sharp from 'sharp'
+import { fileName } from '../fileName.js'
 
 export async function linker(result, media) {
-  const zipped = new JSZip();
-  let imgFolder = zipped.folder("Images");
-  let xmlFolder = zipped.folder("XML");
+	const zipped = new JSZip()
+	let imgFolder = zipped.folder('Images')
+	let xmlFolder = zipped.folder('XML')
 
-  const xmlFileNames = Object.keys(result.XML)
-  const imgFileNames = Object.keys(media)
+	const xmlFileNames = Object.keys(result.XML)
+	const imgFileNames = Object.keys(media)
 
-  for (let filename of xmlFileNames) {
-    xmlFolder.file(filename, result.XML[filename])
-
-  }
-  for (let fullName of imgFileNames) {
-    let name = fileName(fullName)
-    let extension = name.split('.').pop()
-    let fileData = await media[fullName].async('arraybuffer')
-
-    if (extension !== 'jpeg' && extension !== 'jpg') {
-      name = name.slice(0, -extension.length) + 'jpeg'
-      if (extension === 'emf') {
-        //const buffer = Buffer.from(fileData);
-        //const svg = convertEmfToSvg(buffer);
-        //const jpegData = await sharpConverter(svg)
-        imgFolder.file(result.Images[name], fileData); // temp
-      }
-      else {
-        const jpegData = await sharpConverter(fileData)
-        imgFolder.file(result.Images[name], jpegData);
-      }
-    }
-    else {
-      imgFolder.file(result.Images[name], fileData);
-    }
-  }
-  //console.log(imgFileNames)
-  for (let fullName of imgFileNames) {
-    let name = fileName(fullName)
-    let fileData = await media[fullName].async('arraybuffer')
-    //console.log(name)
-    if (result.Images[name] !== undefined)
-      imgFolder.file(result.Images[name], fileData)
-  }
-  return zipped;
+	for (let filename of xmlFileNames) {
+		xmlFolder.file(filename, result.XML[filename])
+	}
+	//console.log(imgFileNames)
+	for (let fullName of imgFileNames) {
+		let name = fileName(fullName)
+		let extension = name.split('.').pop()
+		let fileData = await media[fullName].async('arraybuffer')
+		//console.log(name)
+		if (extension !== 'jpeg' && extension !== 'jpg') {
+			if (extension === 'emf') {
+				if (result.Images[name] !== undefined) {
+          imgFolder.file(result.Images[name], fileData);
+        }
+			} else {
+				// png
+        await sharp(fileData)
+					.toFormat('jpeg') //	.jpeg({quality: 100})
+					.toBuffer()
+					.then(jpegData => {
+            if (result.Images[name] !== undefined) {
+              imgFolder.file(result.Images[name], jpegData)
+            }
+          })
+          .catch( err => {
+            console.error( err )
+          })
+			}
+		} else {
+			if (result.Images[name] !== undefined) {
+				imgFolder.file(result.Images[name], fileData)
+			}
+		}
+	}
+	return zipped
 }
-
-
